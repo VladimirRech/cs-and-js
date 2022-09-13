5e9c4f35acb203cd389c9807744d99c9f840a744
Vladimir Rech <rechvladimir@hotmail.com>
2022-09-12 21:35:42 -0300

    Improving test case.

--- study-csharp-base.tests/HackerHank_tests.cs
+++ study-csharp-base.tests/HackerHank_tests.cs
@@ -32,4 +32,19 @@ public class HackerHank_tests
         int returned = study_hello.classes.HackerHank.LonelyInteger(lst);
         Assert.IsTrue(expected == returned, "It should be 4.");
     }
+
+    [TestCase("0, 1, 2, 3, 4, 5, 4, 3, 2, 1, 0", 5)]
+    [TestCase("10, 11, 11, 9, 8, 10, 9", 8)]
+    public void HackerHank_LonelyInteger_IsTrueLonelyIntMany(string given, int expected)
+    {
+        List<int> lst = new List<int>();
+
+        foreach(string item in given.Split(','))
+        {
+            lst.Add(int.Parse(item));
+        }
+
+        int returned = study_hello.classes.HackerHank.LonelyInteger(lst);
+        Assert.IsTrue(returned == expected, $"It should be {expected} it is {returned}");
+    }
 }
