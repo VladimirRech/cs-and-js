93994f2144f859056e70c019cf7ca6a16dfc091a
Vladimir Rech <rechvladimir@hotmail.com>
2023-05-23 21:22:32 -0300

    Adding new project.

---
+++ LineCount/Classes/FileLinesApp.cs
@@ -0,0 +1,12 @@
+//
+// Read a text file
+// Return a list with: Duplicated Line, positions where it is
+//
+
+public class FileLinesApp 
+{
+	public string DoSomething()
+	{
+		return "I didn't do it!";
+	}
+}
