e2b15f22833cde79333c6c616bd6eb6b8dd2dd4c
Vladimir Rech <rechvladimir@hotmail.com>
2023-06-08 16:39:49 -0300

    Class to model the layout

---
+++ CheckTextFileLayout/LayoutFile.cs
@@ -0,0 +1,9 @@
+using System;
+
+public struct LayoutFile 
+{
+	public string Code { get; set; }
+	public int Length { get; set; }
+	public bool Found { get; set; }
+	public bool SizeMatch { get; set; }
+}
